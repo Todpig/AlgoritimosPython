5cf588f55575581f1548dec695466cf39b8a17a4
LUCAS RAFAEL DA SILVA COSTA <20221094040035@ifrn.local>
2022-11-22 14:54:14 -0300

    Envio das formas de ordenação MergeSort

---
+++ ALGORITIMO/mergesort.py
@@ -0,0 +1,40 @@
+import random
+import time
+
+
+def mergesort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista)
+    if(fim - inicio > 1):
+        meio = (fim + inicio)//2
+        mergesort(lista, inicio, meio)
+        mergesort(lista, meio, fim)
+        merge(lista, inicio, meio, fim)
+
+def merge(lista, inicio, meio, fim):
+    left = lista[inicio:meio]
+    right = lista[meio:fim]
+    i, j = 0, 0
+    for k in range(inicio, fim):
+        if i >= len(left):
+            lista[k] = right[j]
+            j = j + 1
+        elif j >= len(right):
+            lista[k] = left[i]
+            i = i + 1
+        elif left[i] < right[j]:
+            lista[k] = left[i]
+            i = i + 1
+        else:
+            lista[k] = right[j]
+            j = j + 1
+        
+any_numbers = random.sample(range(1, 100), 50)
+
+if __name__ == '__main__':
+    lista = any_numbers
+    inicio = time.time()
+    mergesort(lista)
+    fim = time.time()
+    print(lista)
+    print('o tempo de execução é: ', fim - inicio)
---
+++ ALGORITIMO/primosmerge.py
@@ -0,0 +1,57 @@
+import random
+import time
+
+primos = []
+
+def verifica_primo(n):
+    mult = 0
+    for count in range(2,n):
+        if (n % count == 0):
+            mult += 1
+
+    if(mult==0):
+        primos.append(n)   
+
+def mergesort(lista, inicio=0, fim=None):
+    if fim is None:
+        fim = len(lista)
+    if(fim - inicio > 1):
+        meio = (fim + inicio)//2
+        mergesort(lista, inicio, meio)
+        mergesort(lista, meio, fim)
+        merge(lista, inicio, meio, fim)
+
+        
+def merge(lista, inicio, meio, fim):
+    left = lista[inicio:meio]
+    right = lista[meio:fim]
+    i, j = 0, 0
+    for k in range(inicio, fim):
+        if i >= len(left):
+            lista[k] = right[j]
+            verifica_primo(lista[k])
+            j = j + 1
+        elif j >= len(right):
+            lista[k] = left[i]
+            verifica_primo(lista[k])
+            i = i + 1
+        elif left[i] < right[j]:
+            lista[k] = left[i]
+            verifica_primo(lista[k])
+            i = i + 1
+        else:
+            lista[k] = right[j]
+            verifica_primo(lista[k])
+            j = j + 1
+
+any_numbers = random.sample(range(1, 1000000000), 10000000)
+
+if __name__ == '__main__':
+    lista = any_numbers
+    
+    inicio = time.time()
+    mergesort(lista)
+    fim = time.time()
+    
+    print(len(primos))
+    print('O tempo de execução é: ', fim - inicio)
